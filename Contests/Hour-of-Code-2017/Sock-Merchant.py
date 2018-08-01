cab1de3e1278171c47857af848a810244186707d
Justin Teng <justintteng@gmail.com>
2018-08-01 16:03:37 -0500

    Create Sock-Merchant.py

---
+++ Contests/Hour-of-Code-2017/Sock-Merchant.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    c = Counter()
+    for a in ar:
+        c[a] = c.get(a, 0) + 1
+    count = 0
+    for v in c.values():
+        count += v // 2
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
