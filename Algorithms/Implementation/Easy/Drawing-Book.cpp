2fe2e66a79a8a2f6e8bdcb955cb64f5ad9481987
Justin Teng <justintteng@gmail.com>
2018-08-09 21:53:52 -0500

    Create Drawing-Book.cpp

---
+++ Algorithms/Implementation/Easy/Drawing-Book.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solve(int n, int p){
+    return min(p/2, n/2 - p/2);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int p;
+    cin >> p;
+    int result = solve(n, p);
+    cout << result << endl;
+    return 0;
+}
