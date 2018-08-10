d29d02000cf07ab43af79e8af8e5978e56ad966f
Justin Teng <justintteng@gmail.com>
2018-08-09 22:15:18 -0500

    Create Taum-and-Bday.cpp

---
+++ Algorithms/Implementation/Easy/Taum-and-Bday.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the taumBday function below.
+long taumBday(long b, long w, long bc, long wc, long z) {
+    long ans;
+    if (bc + z < wc) {
+        ans = b*bc + w*(bc+z);
+    } else if (wc + z < bc) {
+        ans = w*wc + b*(wc+z);
+    } else {
+        ans = b*bc + w*wc;
+    }
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string bw_temp;
+        getline(cin, bw_temp);
+
+        vector<string> bw = split_string(bw_temp);
+
+        int b = stol(bw[0]);
+
+        int w = stol(bw[1]);
+
+        string bcWcz_temp;
+        getline(cin, bcWcz_temp);
+
+        vector<string> bcWcz = split_string(bcWcz_temp);
+
+        int bc = stol(bcWcz[0]);
+
+        int wc = stol(bcWcz[1]);
+
+        int z = stol(bcWcz[2]);
+
+        long result = taumBday(b, w, bc, wc, z);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
