c278c3134cb871cca5defbe32eee3cf5894a903c
Justin Teng <justintteng@gmail.com>
2018-08-15 22:45:04 -0500

    Create Input-and-Output.cpp

---
+++ Cpp/Introduction/Input-and-Output.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    x += y;
+    cin >> y;
+    cout << x + y << endl;
+    return 0;
+}
