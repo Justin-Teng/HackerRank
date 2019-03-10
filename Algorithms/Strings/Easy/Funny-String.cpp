62f5098503284988f7a1ac1a9c353ff8118c38ba
Justin Teng <justintteng@gmail.com>
2019-03-09 20:10:34 -0600

    Create Funny-String.cpp

---
+++ Algorithms/Strings/Easy/Funny-String.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the funnyString function below.
+string funnyString(string s) {
+    for (int i = 0; i < s.size()-1; i++) {
+        if (abs(s.at(i) - s.at(i+1)) != abs(s.at(s.size()-i-1) - s.at(s.size()-i-2)))
+            return "Not Funny";
+    }
+    return "Funny";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        string result = funnyString(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
