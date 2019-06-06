30a9ee576e880733e88a3a888a2ddacba074f4c6
Justin Teng <justintteng@gmail.com>
2019-06-06 15:49:13 -0500

    Create Day-1-Functions.js

---
+++ Tutorials/10-Days-of-Javascript/Day-1-Functions.js
@@ -0,0 +1,31 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function factorial(n) {
+    let x = n;
+    let product = n;
+    while (x-- > 1)
+        product *= x;
+    return product;
+}
