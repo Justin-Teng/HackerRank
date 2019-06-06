2005cbd6e034c78b985e1fd15ce2791154471334
Justin Teng <justintteng@gmail.com>
2019-06-06 15:54:52 -0500

    Create Day-5-Template-Literals.js

---
+++ Tutorials/10-Days-of-Javascript/Day-5-Template-Literals.js
@@ -0,0 +1,18 @@
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ * 
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function sides(literals, ...expressions) {
+    let A = expressions[0];
+    let P = expressions[1];
+    let temp = Math.sqrt(P*P - 16 * A);
+    let s1 = (P - temp) / 4;
+    let s2 = (P + temp) / 4;
+    
+    return [s1, s2];
+}
