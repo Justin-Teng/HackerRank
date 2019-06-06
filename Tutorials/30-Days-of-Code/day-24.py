b5387e3cbc10a301062e75ff2ed3fb16a50b3946
Justin Teng <justintteng@gmail.com>
2019-06-06 13:46:35 -0500

    Create day-24.py

---
+++ Tutorials/30-Days-of-Code/day-24.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def insert(self,head,data):
+            p = Node(data)           
+            if head==None:
+                head=p
+            elif head.next==None:
+                head.next=p
+            else:
+                start=head
+                while(start.next!=None):
+                    start=start.next
+                start.next=p
+            return head  
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def removeDuplicates(self,head):
+        it = head
+        while it and it.next:
+            data1 = it.data
+            it2 = it.next
+            while data1 == it2.data:
+                if not it2.next:
+                    it.next = None
+                    return head
+                
+                it2 = it2.next
+            
+            it.next = it2
+            it = it2
+        
+        return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+head=mylist.removeDuplicates(head)
+mylist.display(head); 
