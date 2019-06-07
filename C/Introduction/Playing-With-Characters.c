7d919cefa27f7c6419caaf2de62b838d2ddc5c23
Justin Teng <justintteng@gmail.com>
2019-06-07 15:42:06 -0500

    Create Playing-With-Characters.c

---
+++ C/Introduction/Playing-With-Characters.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char c;
+    scanf("%c", &c);
+    scanf("\n");
+    
+    char string[256];
+    scanf("%s", string);
+    scanf("\n");
+
+    char* sentence = NULL;
+    size_t size;
+    getline(&sentence, &size, stdin);
+
+    printf("%c\n", c);
+    printf("%s\n", string);
+    printf("%s\n", sentence);
+    return 0;
+}
+
