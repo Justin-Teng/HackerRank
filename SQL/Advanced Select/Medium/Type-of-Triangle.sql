12b491ef7c68f631d622c2ae74512e8253424f47
Justin Teng <justintteng@gmail.com>
2019-06-12 13:13:49 -0500

    Delete Type-of-Triangle.sql

--- SQL/Advanced Select/Medium/Type-of-Triangle.sql
+++
@@ -1,5 +0,0 @@
-SELECT IF(A+B<=C OR A+C<=B OR B+C<=A, 'Not A Triangle',
-          IF(A=B AND B=C, 'Equilateral',
-             IF(A!=B AND B!=C AND A!=C, 'Scalene',
-                'Isosceles')))
-FROM Triangles;
